f4dc9ca23375a237d96645cb487ec51558fee052
m.falahatnejad <m.falahatnejad@tosantechno.net>
2023-12-18 14:46:56 +0330

    add lazy and suspense

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,16 +1,45 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import * as pathName from "./paths";
-import AxiosExample from "./../pages/AxiosExample/AxiosExample";
-import FetchExample from "./../pages/FetchExample/FetchExample";
-import GraphQlExample from "./../pages/GraphQlExample/GraphQlExample";
+const AxiosExample = lazy(() => import("./../pages/AxiosExample/AxiosExample"));
+const FetchExample = lazy(() => import("./../pages/FetchExample/FetchExample"));
+const GraphQlExample = lazy(() => import("./../pages/GraphQlExample/GraphQlExample"));
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path={pathName.axiosExample} element={<AxiosExample />} />
-      <Route path={pathName.fetchExample} element={<FetchExample />} />
-      <Route path={pathName.graphQlExample} element={<GraphQlExample />} />
-      <Route path="*" element={<p>404</p>} />
+    <Routes Suspense={<p>loading</p>}>
+      <Route
+        path={pathName.axiosExample}
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <AxiosExample />
+          </Suspense>
+        }
+      />
+      <Route
+        path={pathName.fetchExample}
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <FetchExample />
+          </Suspense>
+        }
+      />
+      <Route
+        path={pathName.graphQlExample}
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <GraphQlExample />
+          </Suspense>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <p>404</p>
+          </Suspense>
+        }
+      />
     </Routes>
   );
 };
