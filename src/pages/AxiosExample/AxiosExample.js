eb017aa1b24623fa093d4f7d013d8ec5d9ed9664
m.falahatnejad <m.falahatnejad@tosantechno.net>
2023-12-19 19:04:39 +0330

    add axios and fetch examples

--- src/pages/AxiosExample/AxiosExample.js
+++ src/pages/AxiosExample/AxiosExample.js
@@ -1,5 +1,23 @@
+import useSWR from "swr";
+import axios from "axios";
+
 const AxiosExample = () => {
-  return <>AxiosExample</>;
+  const fetcher = (url) => axios.get(url).then((res) => res.data);
+  const { data, error, isLoading } = useSWR("https://jsonplaceholder.typicode.com/users", fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  return (
+    <>
+      <h1>Axios Example </h1>
+      <h2>this is result of a fake API </h2>
+      <div>
+        {data.map((data) => {
+          return <p key={data.id}>{data.name}</p>;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default AxiosExample;
--- src/pages/FetchExample/FetchExample.js
+++ src/pages/FetchExample/FetchExample.js
@@ -1,5 +1,22 @@
+import useSWR from "swr";
+
 const FetchExample = () => {
-  return <>FetchExample</>;
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data, error, isLoading } = useSWR("https://jsonplaceholder.typicode.com/users", fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  return (
+    <>
+      <h1>Fetch Example </h1>
+      <h2>this is result of a fake API </h2>
+      <div>
+        {data.map((data) => {
+          return <p key={data.id}>{data.name}</p>;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default FetchExample;
